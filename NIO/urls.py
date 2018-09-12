2572f0104a190e94f278d01ea4f1d27142e81307
ybbiubiubiu <394980551@qq.com>
2018-09-12 23:06:17 +0800

    Revert "v"
    This reverts commit 35f77890c750d2ac757d93e072ba015767128904.

--- NIO/urls.py
+++ NIO/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from app01 import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^ranking/', views.rank),
 ]
--- app01/admin.py
+++ app01/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from app01.models import Examination_Record,UserInfo
 
 # Register your models here.
-admin.site.register(Examination_Record)
-admin.site.register(UserInfo)
