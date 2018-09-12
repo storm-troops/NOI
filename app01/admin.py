35f77890c750d2ac757d93e072ba015767128904
ybbiubiubiu <394980551@qq.com>
2018-09-12 23:04:41 +0800

    v
    添加了一个基板  配置了静态文件以及模板

--- app01/admin.py
+++ app01/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from app01.models import Examination_Record,UserInfo
 
 # Register your models here.
+admin.site.register(Examination_Record)
+admin.site.register(UserInfo)
--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.db.models import F,Q
+from app01 import models
 
 # Create your views here.
+
+
+def rank(requset):
+    if requset.method == "GET":
+
+        #从数据库拉取用户的答题记录，根据胜率作排名，展现于前端页面。
+        rank_list = models.Examination_Record.objects.order_by(F('pass_count')/F('examination_count'))
+        return render(requset,"rank.html",{"rank_list":rank_list})
